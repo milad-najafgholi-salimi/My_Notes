6dd9c1347591e58f900dd090fb3ce791156e5b8d
Milad.Najafgholi.Salimi <milad.najafgholi.salimi@gmail.com>
2025-11-04 21:43:48 +0330

    Add files via upload

---
+++ DSA/Linear_Search.py
@@ -0,0 +1,22 @@
+"""
+It's really simple and doesn't need extra explain.
+"""
+
+def linear_search(arr, target_value):
+    for i in range(len(arr)):
+        if arr[i] == target_value:
+            return i
+    return -1
+
+
+# Example:
+
+array = [3, 7, 2, 9, 5]
+target_value = 9
+
+result = linear_search(array, target_value)
+
+if result != -1:
+    print(result)
+else:
+    print("Not found")
