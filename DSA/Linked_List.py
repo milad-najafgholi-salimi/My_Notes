58fca2c7891ccbbe5295200412f6bf9b1f28a3c3
Milad.Najafgholi.Salimi <milad.najafgholi.salimi@gmail.com>
2025-11-06 21:37:57 +0330

    Add files via upload

---
+++ DSA/Linked_List.py
@@ -0,0 +1,174 @@
+"""
+I wrote about "Linked Lists" in my notebook, so I don't need to write over again here.
+And also it's really easy.
+
+As I wrote in my "DSA" notebook, there is NOT shifting problem in linked list while 
+adding and deleting an element like in arrays(= lists).
+"""
+
+# Traversal of a singly linked list in Python:
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def traverse_and_print(head):
+    current_node = head
+    while current_node:
+        print(current_node.data, end=" -> ")
+        current_node = current_node.next
+    print("null")
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+traverse_and_print(node1)
+
+
+
+
+
+"""
+Finding the lowest value in a singly linked list in Python:
+"""
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+def findLowestValue(head):
+  minValue = head.data
+  currentNode = head.next
+  while currentNode: # while it doesen't reach None (becaluse bool of None is False)
+    if currentNode.data < minValue:
+      minValue = currentNode.data
+    currentNode = currentNode.next # if the previous if statement was false, continue the next value until reach the end (= reach to None)
+  return minValue
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+print("The lowest value in the linked list is:", findLowestValue(node1))
+
+
+
+
+
+"""
+Deleting a specific node in a singly linked list in Python:
+"""
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+def traverseAndPrint(head):
+  currentNode = head
+  while currentNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.next
+  print("null")
+
+def deleteSpecificNode(head, nodeToDelete):
+  if head == nodeToDelete:
+    return head.next
+
+  currentNode = head
+  while currentNode.next and currentNode.next != nodeToDelete:
+    currentNode = currentNode.next
+
+  if currentNode.next is None:
+    return head
+
+  currentNode.next = currentNode.next.next
+
+  return head
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+print("Before deletion:")
+traverseAndPrint(node1)
+
+# Delete node4
+node1 = deleteSpecificNode(node1, node4)
+
+print("\nAfter deletion:")
+traverseAndPrint(node1)
+
+
+
+
+
+"""
+Inserting a node in a singly linked list in Python:
+"""
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+def traverseAndPrint(head):
+  currentNode = head
+  while currentNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.next
+  print("null")
+
+def insertNodeAtPosition(head, newNode, position):
+  if position == 1:
+    newNode.next = head
+    return newNode
+
+  currentNode = head
+  for _ in range(position - 2):
+    if currentNode is None:
+      break
+    currentNode = currentNode.next
+
+  newNode.next = currentNode.next
+  currentNode.next = newNode
+  return head
+
+node1 = Node(7)
+node2 = Node(3)
+node3 = Node(2)
+node4 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+print("Original list:")
+traverseAndPrint(node1)
+
+# Insert a new node with value 97 at position 2
+newNode = Node(97)
+node1 = insertNodeAtPosition(node1, newNode, 2)
+
+print("\nAfter insertion:")
+traverseAndPrint(node1)
